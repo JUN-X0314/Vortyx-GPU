2c2d512dc3e489165577c4c0da94c63cfd51f0ab
신준호 <junhoverse@gmail.com>
2026-09-07 07:19:32 +0900

    Phase 16.0.1: bump core version to 0.16.1

--- src/core/version.hpp
+++ src/core/version.hpp
@@ -2,9 +2,9 @@
 
 #define VORTYX_VERSION_MAJOR 0
 #define VORTYX_VERSION_MINOR 16
-#define VORTYX_VERSION_PATCH 0
+#define VORTYX_VERSION_PATCH 1
 
-#define VORTYX_VERSION_STRING "0.16.0"
+#define VORTYX_VERSION_STRING "0.16.1"
 
 inline constexpr int vortyx_version_major() { return VORTYX_VERSION_MAJOR; }
 inline constexpr int vortyx_version_minor() { return VORTYX_VERSION_MINOR; }
